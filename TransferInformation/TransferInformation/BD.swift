639027b3a08e68d70cfedbab784337e3a34b05c1
intern <intern@admins-iMac.local>
2017-07-06 15:17:38 +0300

    Test BD on realmswift

---
+++ TransferInformation/TransferInformation/BD.swift
@@ -0,0 +1,15 @@
+//
+//  BD.swift
+//  TransferInformation
+//
+//  Created by intern on 06/07/2017.
+//  Copyright © 2017 intern. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class BD: Object {
+    
+    dynamic var name = ""
+}
---
+++ TransferInformation/TransferInformation/DataBase.swift
@@ -0,0 +1,21 @@
+//
+//  DataBase.swift
+//  TransferInformation
+//
+//  Created by intern on 06/07/2017.
+//  Copyright © 2017 intern. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Event: Object {
+    
+    dynamic var idCard = 0
+    dynamic var idLayout = 0
+    dynamic var Description = ""
+}
+
+class Tree: Object{
+    
+}
